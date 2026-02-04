439c370d0969a7c460cd2d3632fc11bc5dc9493d
Hector <hocht@lacuevalab.studio>
2026-02-04 16:03:26 -0600

    fix: update feature section icons for better visual distinction
    - Changed 'Ingeniería Inversa' to use 'tools' icon (herramientas)
    - Updated 'Stock Programado' to use 'books' icon (libro)
    - Set 'Trato Personal' to use 'frame' icon (marco)
    - Kept 'Certeza Técnica' with 'verified' icon (verificado)
    - Ensured all icons are properly displayed with correct styling
    - Fixed icon duplication issues in the features section
    
    These changes improve the visual hierarchy and make each feature more distinguishable with appropriate iconography.

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -2,7 +2,7 @@ export const Icons = {
   groups: {
     paths: [
       {
-        d: "m150-400 82-80-82-82-80 82 80 80Zm573-10 87-140 88 140H723Zm-243-70q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm.351-180Q455-660 437.5-642.851t-17.5 42.5Q420-575 437.351-557.5t43 17.5Q506-540 523-557.351t17-43Q540-626 522.851-643t-42.5-17ZM480-600ZM0-240v-53q0-39.464 42-63.232T150.398-380q12.158 0 23.38.5T196-377.273q-8 17.273-12 34.842-4 17.57-4 37.431v65H0Zm240 0v-65q0-65 66.5-105T480-450q108 0 174 40t66 105v65H240Zm570-140q67.5 0 108.75 23.768T960-293v53H780v-65q0-19.861-3.5-37.431Q773-360 765-377.273q11-1.727 22.171-2.227 11.172-.5 22.829-.5Zm-330.2-10Q400-390 350-366q-50 24-50 61v5h360v-6q0-36-49.5-60t-130.7-24Zm.2 90Z",
+        d: "m150-400 82-80-82-82-80 82 80 80 80Zm573-10 87-140 88 140H723Zm-243-70q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm.351-180Q455-660 437.5-642.851t-17.5 42.5Q420-575 437.351-557.5t43 17.5Q506-540 523-557.351t17-43Q540-626 522.851-643t-42.5-17ZM480-600ZM0-240v-53q0-39.464 42-63.232T150.398-380q12.158 0 23.38.5T196-377.273q-8 17.273-12 34.842-4 17.57-4 37.431v65H0Zm240 0v-65q0-65 66.5-105T480-450q108 0 174 40t66 105v65H240Zm570-140q67.5 0 108.75 23.768T960-293v53H780v-65q0-19.861-3.5-37.431Q773-360 765-377.273q11-1.727 22.171-2.227 11.172-.5 22.829-.5Zm-330.2-10Q400-390 350-366q-50 24-50 61v5h360v-6q0-36-49.5-60t-130.7-24Zm.2 90Z",
       },
     ],
     class: "mt-1 h-8 w-8 shrink-0 fill-orange-400 dark:fill-orange-300",
@@ -50,7 +50,7 @@ export const Icons = {
       },
     ],
     class:
-      "mt-2 h-6 w-6 shrink-0 fill-neutral-700 hs-tab-active:fill-orange-400 dark:fill-neutral-300 dark:hs-tab-active:fill-orange-300 md:h-7 md:w-7",
+      "mt-2 h-6 w-6 shrink-0 fill-neutral-700 hs-tab-active:fill-micrafil-blue-600 dark:fill-neutral-300 dark:hs-tab-active:fill-micrafil-blue-500 md:h-7 md:w-7",
     width: 48,
     height: 48,
     viewBox: "0 -960 960 960",
@@ -62,7 +62,7 @@ export const Icons = {
       },
     ],
     class:
-      "mt-2 h-6 w-6 shrink-0 fill-neutral-700 hs-tab-active:fill-orange-400 dark:fill-neutral-300 dark:hs-tab-active:fill-orange-300 md:h-7 md:w-7",
+      "mt-2 h-6 w-6 shrink-0 fill-neutral-700 hs-tab-active:fill-micrafil-blue-600 dark:fill-neutral-300 dark:hs-tab-active:fill-micrafil-blue-500 md:h-7 md:w-7",
     width: 48,
     height: 48,
     viewBox: "0 -960 960 960",
@@ -74,7 +74,7 @@ export const Icons = {
       },
     ],
     class:
-      "h-6 w-6 shrink-0 text-neutral-700 hs-tab-active:text-orange-400 dark:text-neutral-300 dark:hs-tab-active:text-orange-300 md:h-7 md:w-7",
+      "h-6 w-6 shrink-0 text-neutral-700 hs-tab-active:text-micrafil-blue-600 dark:text-neutral-300 dark:hs-tab-active:text-micrafil-blue-500 md:h-7 md:w-7",
     width: 24,
     height: 24,
     viewBox: "0 0 24 24",
@@ -93,7 +93,7 @@ export const Icons = {
         d: "M12 19V5",
       },
     ],
-    class: "h-5 w-5 shrink-0 text-orange-400 dark:text-orange-300",
+    class: "h-5 w-5 shrink-0 text-micrafil-blue-600 dark:text-micrafil-blue-500",
     width: 24,
     height: 24,
     viewBox: "0 0 24 24",
